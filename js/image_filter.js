2bbee95175a3e4c697574122876cdb5557a48bed
RandomAshes <randomashes@gmail.com>
2016-03-08 12:57:09 -0500

    Version 1
    Version with my image filter + jquery lightbox plugin

--- js/image_filter.js
+++ js/image_filter.js
@@ -1,11 +1,31 @@
+(function() {																	// Lives in a IIFE
+	var $imgs = $('#gallery a');							// Get the images
+	var $search = $('#filter_search');					// Get the input element
+	var cache = [];															// Create an array called cache
 
-// Set up events to check when the user types in searchbox
-function imagefilter(form, a) {
+	$imgs.each(function() {											// For each image
+		cache.push({															// Add an object to the cache array
+			element: this,													// This image
+			text: this.title.trim().toLowerCase()			// It's alt text (lowercase trimmed)
+		});
+	});
 
+	function filter() {														// Declare filter() function
+		var query = this.value.trim().toLowerCase();// Get the query
 
-	
-}
+		cache.forEach(function(img) {								// For each entry in cache pass image
+				var index = 0;													// Set index to 0
+				if (query) {														// If there is some query text
+					index = img.text.indexOf(query);			// Find if query text is in there
+				}
 
-// Check the value of the input
-// Compare against all images (actually all links) Title Attribute
-// Hide the non-matching images, while showing the matching ones
+				img.element.style.display = index === -1 ? 'none' : ''; // Show / hide
+		})
+	}
+
+	if ('oninput' in $search[0]) {								// If browser supports input event
+		$search.on('input', filter);								// Use input event to call filter()
+	} else {																			// Otherwise
+		$search.on('keyup', filter);								// Use keyup event to call filter()
+	}																				
+}());
