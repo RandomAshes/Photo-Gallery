4bf023d6324ee7e1f6d61a206374b897f632a200
RandomAshes <randomashes@gmail.com>
2016-03-19 12:05:24 -0400

    Made comments more readable

--- js/image_filter.js
+++ js/image_filter.js
@@ -1,31 +1,50 @@
-(function() {																	// Lives in a IIFE
-	var $imgs = $('#gallery a');							// Get the images
-	var $search = $('#filter_search');					// Get the input element
-	var cache = [];															// Create an array called cache
+// Lives in a IIFE
+(function() {			
+	// Get the images														
+	var $imgs = $('#gallery a');
+	// Get the input element							
+	var $search = $('#filter_search');
+	// Create an array called cache					
+	var cache = [];		
 
-	$imgs.each(function() {											// For each image
-		cache.push({															// Add an object to the cache array
-			element: this,													// This image
-			text: this.title.trim().toLowerCase()			// It's alt text (lowercase trimmed)
+	// For each image													
+	$imgs.each(function() {		
+		// Add an object to the cache array
+		cache.push({
+			// This image															
+			element: this,							
+			// It's alt text (lowercase trimmed)						
+			text: this.title.trim().toLowerCase()			
 		});
 	});
 
-	function filter() {														// Declare filter() function
-		var query = this.value.trim().toLowerCase();// Get the query
-
-		cache.forEach(function(img) {								// For each entry in cache pass image
-				var index = 0;													// Set index to 0
-				if (query) {														// If there is some query text
-					index = img.text.indexOf(query);			// Find if query text is in there
+	// Declare filter() function
+	function filter() {				
+		// Get the query										
+		var query = this.value.trim().toLowerCase();
+		
+		// For each entry in cache pass image
+		cache.forEach(function(img) {		
+				// Set index to 0						
+				var index = 0;	
+				// If there is some query text												
+				if (query) {							
+					// Find if query text is in there							
+					index = img.text.indexOf(query);			
 				}
 
-				img.element.style.display = index === -1 ? 'none' : ''; // Show / hide
+				// Show / hide
+				img.element.style.display = index === -1 ? 'none' : ''; 
 		});
 	}
 
-	if ('oninput' in $search[0]) {								// If browser supports input event
-		$search.on('input', filter);								// Use input event to call filter()
-	} else {																			// Otherwise
-		$search.on('keyup', filter);								// Use keyup event to call filter()
+	// If browser supports input event
+	if ('oninput' in $search[0]) {
+		// Use input event to call filter()								
+		$search.on('input', filter);		
+		// Otherwise						
+	} else {									
+		// Use keyup event to call filter()									
+		$search.on('keyup', filter);								
 	}																				
 }());
